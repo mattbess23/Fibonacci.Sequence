9ba677aeb7505467aeb560fd888306a93911ce2d
matthew <matthewbessick23@hotmail.com>
2020-01-13 18:35:11 +0200

    Add project files.

---
+++ Fibonacci Sequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fibonacci_Sequence
+{
+
+    class Program
+    {
+        public static int FibonacciSequence(int n)
+        {
+            int a = 0;
+            int b = 2;
+            // n variable calculates the fibonacci in iteration
+            for (int i = 0; i < n; i++){
+                int calc = a;
+                a = b;
+                b = calc + b;}
+            return a;
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < 5000000; i++){
+                if (i == 5000000)
+                    break;
+
+                Console.WriteLine(FibonacciSequence(i));
+                Console.ReadLine();}
+        }
+    }
+}
+
+
